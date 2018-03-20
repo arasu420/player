dde09d88f13f421a36bce7f26e1c4f4166417198
arasu420 <36299277+arasu420@users.noreply.github.com>
2018-03-20 17:48:45 +0530

    Create sinvalue.cpp

---
+++ sinvalue.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+  double n,result;
+  cout<<"enter the number ";
+  cin>>n;
+  result=sin(n);
+  cout << "sin value is  = " << result << endl;
+  return 0;
+}
