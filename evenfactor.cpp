9cd4fcba8a0f78970b415468b2b588fa7d6e8bb5
arasu420 <36299277+arasu420@users.noreply.github.com>
2018-03-20 17:40:28 +0530

    Create evenfactor.cpp

---
+++ evenfactor.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+   int n,i;
+   cout<<"enter the number  ";
+   cin>>n;
+   for(i=1;i<=n;i++)
+   {
+       if(i%2==0 && n%i==0)
+       {
+           cout<<i<<"  ";
+       }       
+   }
+	return 0;
+}
