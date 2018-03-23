c9c5c96af920f2b1e993627da5f5f6ebb894969a
arasu420 <36299277+arasu420@users.noreply.github.com>
2018-03-23 11:44:58 +0530

    Create Printkthodd.cpp

---
+++ Printkthodd.cpp
@@ -0,0 +1,27 @@
+ #include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,i,z;
+	cin>>n;
+ int a[n],k;
+ for( i=0;i<n;i++)
+ {
+ cin>>a[i];
+ }
+ cin>>k;
+z=(k*2)-1;
+ 
+ if(k<=(n+1)/2)
+ {
+ 	cout <<a [z-1];
+ 	}
+ 	else
+ 	{
+ 		cout <<"invalid number";
+ 	}
+ return 0;
+}
+    
