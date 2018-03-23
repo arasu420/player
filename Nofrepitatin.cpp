02996255bf378af661295ebf5d4070e60e90bd39
arasu420 <36299277+arasu420@users.noreply.github.com>
2018-03-23 11:18:55 +0530

    Create Nofrepitatin.cpp

---
+++ Nofrepitatin.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,i,count=0;
+	cin>>n;
+ int a[n],k;
+ for( i=0;i<n;i++)
+ {
+ cin>>a[i];
+ }
+ cin>>k;
+for (i=0;i<n;i++)
+{
+	
+ if(k==a [i])
+ {
+ 	count++;
+ 	}
+}
+    cout <<count;
+ return 0;
+}
+    
