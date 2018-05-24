4ad7d7072a75f16fb9f445c9534dabf0ba3c8827
arasu420 <36299277+arasu420@users.noreply.github.com>
2018-05-24 12:18:56 +0530

    Create set8-72.cpp

---
+++ set8-72.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,i;
+    cin>>n;
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    for(i=0;i<n;i++)
+    {
+        if(i==n-1)
+        {
+            cout<<a[i];
+        }
+    }
+    return 0;
+}
